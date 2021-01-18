350aeca0da0b294a0b9452d9cde75f738fcb6984
shivasinghal2000 <singhalshiva936@gmail.com>
2021-01-18 20:03:44 +0530

    trees_session_2

--- Trees/intro.cpp
+++ Trees/intro.cpp
@@ -160,7 +160,7 @@ void levelOrder_01(Node *node)
         if(rvtx->left)
         q.push(rvtx->left);
         if (rvtx->right)
-            q.push(rvtx->right);
+        q.push(rvtx->right);
     }
 }
 
@@ -189,68 +189,301 @@ void levelOrder_02(Node *node)
     }
 }
 
-void levelOrder_03(Node *node)
+vector<vector<int>> levelOrder_03(Node *node)
 {
-    
+     if (!node)
+        return {};
+    queue<Node *> q;
+    q.push(node);
+    vector<vector<int>>v;
+    while (q.size() != 0)
+    {
+        int s=q.size();
+        vector<int>level;
+        while(s-- >0)
+        {
+            Node *rvtx = q.front();
+            q.pop();
+            level.push_back(rvtx->data);
+            if (rvtx->left)
+                q.push(rvtx->left);
+            if (rvtx->right)
+                q.push(rvtx->right);
+        }
+        v.push_back(level);
+    }
+    return v;
 }
 
 //Views.======================================================================
 
-void leftView(Node *node)
+vector<int> leftView(Node *node)
 {
-    
+    if (!node)
+        return {};
+    queue<Node *> q;
+    q.push(node);
+    vector<int> v;
+    while (q.size() != 0)
+    {
+        int s = q.size();
+        int level=-1;
+        while (s-- > 0)
+        {
+            Node *rvtx = q.front();
+            q.pop();
+            if(level==-1)
+            level=(rvtx->data);
+            if (rvtx->left)
+                q.push(rvtx->left);
+            if (rvtx->right)
+                q.push(rvtx->right);
+        }
+        v.push_back(level);
+    }
+    return v;
 }
 
-void rightView(Node *node)
+vector<int> rightView(Node *node)
 {
-    
+    if (!node)
+        return {};
+    queue<Node *> q;
+    q.push(node);
+    vector<int> v;
+    while (q.size() != 0)
+    {
+        int s = q.size();
+        int level = -1;
+        while (s-- > 0)
+        {
+            Node *rvtx = q.front();
+            q.pop();
+            // if (level == -1)
+                level = (rvtx->data);
+            if (rvtx->left)
+                q.push(rvtx->left);
+            if (rvtx->right)
+                q.push(rvtx->right);
+        }
+        v.push_back(level);
+    }
+    return v;
 }
 
-void width(Node *node, int level, pair<int, int> &maxMin)
+void width(Node *node, int index, pair<int, int> &maxmin)
 {
-    
+    if(!node)
+    return;
+    maxmin.first = min(maxmin.first, index);
+    maxmin.second = max(maxmin.second, index);
+    width(node->left,index-1,maxmin);
+    width(node->right, index + 1, maxmin);
 }
 
 void verticalOrderTraversal(Node *node)
 {
-    
+    if(!node)
+    return;
+    pair<int,int>maxmin={0,0};
+    width(node,0,maxmin);
+    int w = maxmin.second - maxmin.first +1;
+    vector<vector<int>>ans(w,vector<int>());
+    queue<pair<Node*,int>>q;
+    q.push({node,-maxmin.first});
+    while(q.size()!=0)
+    {
+        int s=q.size();
+        while(s-- >0)
+        {
+            pair<Node*,int> rpair= q.front();
+            q.pop();
+            ans[rpair.second].push_back(rpair.first->data); 
+            if(rpair.first->left)
+                q.push({rpair.first->left,rpair.second - 1});
+            if (rpair.first->right)
+                q.push({rpair.first->right, rpair.second + 1});
+        }
+    }
+    for(vector<int>&ar:ans)
+    {
+        for(int ele:ar)
+            cout<<ele<<" ";
+        cout<<endl;
+    }
+    cout << endl;
 }
 
 void verticalOrderSum(Node *node)
 {
-    
+    if (!node)
+        return;
+    pair<int, int> maxmin = {0, 0};
+    width(node, 0, maxmin);
+    int w = maxmin.second - maxmin.first + 1;
+    vector<int> ans(w, 0);
+    queue<pair<Node *, int>> q;
+    q.push({node, -maxmin.first});
+    while (q.size() != 0)
+    {
+        int s = q.size();
+        while (s-- > 0)
+        {
+            pair<Node *, int> rpair = q.front();
+            q.pop();
+            ans[rpair.second]+=(rpair.first->data);
+            if (rpair.first->left)
+                q.push({rpair.first->left, rpair.second - 1});
+            if (rpair.first->right)
+                q.push({rpair.first->right, rpair.second + 1});
+        }
+    }
+    for (int ar : ans)
+    {
+            cout << ar << "\n";
+        // cout << endl;
+    }
+    cout << endl;
 }
 
 void verticalView(Node *node)
 {
-    
+    if (!node)
+        return;
+    pair<int, int> maxmin = {0, 0};
+    width(node, 0, maxmin);
+    int w = maxmin.second - maxmin.first + 1;
+    vector<int> ans(w, -1);
+    queue<pair<Node *, int>> q;
+    q.push({node, -maxmin.first});
+    while (q.size() != 0)
+    {
+        int s = q.size();
+        while (s-- > 0)
+        {
+            pair<Node *, int> rpair = q.front();
+            q.pop();
+            if (ans[rpair.second]==-1)
+                ans[rpair.second] = (rpair.first->data);
+            if (rpair.first->left)
+                q.push({rpair.first->left, rpair.second - 1});
+            if (rpair.first->right)
+                q.push({rpair.first->right, rpair.second + 1});
+        }
+    }
+    for (int ar : ans)
+    {
+        cout << ar << " ";
+        // cout << endl;
+    }
+    cout << endl;
 }
 
 void BottomView(Node *node)
 {
-    
+    if (!node)
+        return;
+    pair<int, int> maxmin = {0, 0};
+    width(node, 0, maxmin);
+    int w = maxmin.second - maxmin.first + 1;
+    vector<int> ans(w, -1);
+    queue<pair<Node *, int>> q;
+    q.push({node, -maxmin.first});
+    while (q.size() != 0)
+    {
+        int s = q.size();
+        while (s-- > 0)
+        {
+            pair<Node *, int> rpair = q.front();
+            q.pop();
+            // if (ans[rpair.second] == -1)
+                ans[rpair.second] = (rpair.first->data);
+            if (rpair.first->left)
+                q.push({rpair.first->left, rpair.second - 1});
+            if (rpair.first->right)
+                q.push({rpair.first->right, rpair.second + 1});
+        }
+    }
+    for (int ar : ans)
+    {
+        cout << ar << " ";
+        // cout << endl;
+    }
+    cout << endl;
 }
 
 //Traversal. ===============================================================
 
 Node *rightMost(Node *next, Node *curr)
 {
-    
+    while(next->right && next->right!=curr)
+    {
+        next=next->right;
+    }
+    return next;
 }
 
 void morrisInOrderTraversal(Node *node)
 {
-    
+    Node *curr=node;
+    while(curr)
+    {
+        Node *next= curr->left;
+        if(!next)
+        {
+            cout<<curr->data<<" ";
+            curr=curr->right;
+        }
+        else
+        {
+            Node *rmost=rightMost(next,curr);
+            if(rmost->right==nullptr)  // thread create
+            {
+                rmost->right=curr;  
+                curr=curr->left; // go to left
+            }
+            else
+            {
+                cout<<curr->data<<" "; // lefthas been traversed now print root
+                rmost->right=nullptr; //thread break
+                curr=curr->right; // go to right
+            }
+        }
+    }
 }
 
 void morrisPreOrderTraversal(Node *node)
 {
-    
+    Node *curr = node;
+    while (curr)
+    {
+        Node *next = curr->left;
+        if (!next)
+        {
+            cout << curr->data << " ";
+            curr = curr->right;
+        }
+        else
+        {
+            Node *rmost = rightMost(next, curr);
+            if (rmost->right == nullptr) // thread create
+            {
+                cout<<curr->data<<" ";// root print 
+                rmost->right = curr;
+                curr = curr->left;// go to left
+            }
+            else
+            {
+                rmost->right = nullptr; //thread break
+                curr = curr->right;// go to right
+            }
+        }
+    }
 }
 
 void traversal(Node *root)
 {
-    // morrisInOrderTraversal(root);
+    morrisInOrderTraversal(root);
     // morrisPreOrderTraversal(root);
 }
 
@@ -272,12 +505,28 @@ void set1(Node *root)
     // levelOrder_01(root);
     // levelOrder_02(root);
     // levelOrder_03(root);
-
-    // leftView(root);
+    // cout<<endl;
+    // cout<<"left view\n";
+    // vector<int>v= leftView(root);
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //         cout << v[i] << " ";
+    // }
+    // cout << endl;
+    // cout << "right view\n";
+    // vector<int> v1 = rightView(root);
+    // for (int i = 0; i < v1.size(); i++)
+    // {
+    //     cout << v1[i] << " ";
+    // }
+    // cout << endl;
     // rightView(root);
     // verticalOrderTraversal(root);
+    // verticalOrderSum(root);
     // verticalView(root);
     // BottomView(root);
+    morrisInOrderTraversal(root);
+    morrisPreOrderTraversal(root);
 }
 
 void solve()
@@ -286,7 +535,7 @@ void solve()
     // vector<int> arr = {-15, 5, -8, 2, -1, -1, 6, -1, -1, 1, -1, -1, 6, 3, -1, -1, 9, -1, 0, 4, -1, -1, -2, 10, -1, -1, -1};
 
     Node *root = constructTree(arr);
-    // inOrder(root);
+    inOrder(root);
     // display(root);
     // vector<Node*>path;
     // cout<<rootToNodePath(root,1300,path)<<endl;
@@ -300,9 +549,18 @@ void solve()
     // cout << maximum(root) << endl;
     // cout << minimum(root) << endl;
     // cout << diameter(root) << endl;
-    levelOrder_01(root);
+    // levelOrder_01(root);
     cout<<endl;
-    levelOrder_02(root);
+    // levelOrder_02(root);
+    // cout << endl;
+    // vector<vector<int>> level = levelOrder_03(root);
+    // for(int i=0;i<level.size();i++)
+    // {
+    //     for(int j=0;j<level[i].size();j++)
+    //     cout<<level[i][j]<<" ";
+    //     cout<<endl;
+    // }
+    set1(root);
     // traversal(root);
 }
 
